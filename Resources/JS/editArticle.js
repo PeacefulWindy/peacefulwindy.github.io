fc032b53cb1e14b68557174da7a913c516d25bf0
PeacefulWindy <a485323759@gmail.com>
2019-10-21 14:53:20 +0800

    fix editArticle.js bug

--- Resources/JS/editArticle.js
+++ Resources/JS/editArticle.js
@@ -10,7 +10,7 @@ var editor=new E("#editor");
 editor.create();
 
 $("#submitArticle").on("click",function(){
-    var title=$("#articleTitle").val();
+    var title=$("#title").val();
     var content=editor.txt.html();
     var public=false;
 
