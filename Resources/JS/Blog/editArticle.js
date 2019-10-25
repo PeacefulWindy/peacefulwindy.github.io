f44bb9fa9985aa8ae5faed22a157c5290bd1a7c7
PeacefulWindy <a485323759@gmail.com>
2019-10-25 20:35:29 +0800

    fix more bug

--- Resources/JS/Blog/editArticle.js
+++ Resources/JS/Blog/editArticle.js
@@ -1,52 +1,63 @@
-var token=window.localStorage.getItem("token");
-if(!token)
+window.onload=function()
 {
-    alert("您没有登录!");
-    previousPage();
-}
+    var token=window.localStorage.getItem("token");
 
-var E=window.wangEditor;
-var editor=new E("#editor");
-editor.create();
-
-$("#submitArticle").on("click",function(){
-    var title=$("#title").val();
-    var content=editor.txt.html();
-    var public=false;
-
-    if($("input[name='public']:checked").val()==1)
+    if(!token)
     {
-        public=true;
+        alert("您没有登录!");
+        previousPage();
     }
-    var classify=$("#classify").val()
-    
-    data={
-        "token":token,
-        "title":title,
-        "content":content,
-        "classifyID":classify,
-        "public":public,
+    getClassify("option");
+    id=GetQueryString("id");
+    if(id)
+    {
+        $("#id").val(id);
+        getArticleWithID(id,true);
     }
+    var E=window.wangEditor;
+    var editor=new E("#editor");
+    editor.create();
 
-    $.ajax({
-        type:"POST",
-        Accept: "application/json; charset=utf-8",
-        url:host+"v1/blog/article",
-        dataType:"json",
-        data:JSON.stringify(data),
-        success:function(json)
+    $("#submitArticle").on("click",function(){
+        var title=$("#title").val();
+        var content=editor.txt.html();
+        var public=false;
+    
+        if($("input[name='public']:checked").val()==1)
         {
-            if(json.code==200)
-            {
-                alert("发布成功!");
-            }
-            else
-            {
-                alert("发布失败!\n原因:"+json.error)
-            }
-        },error:function(json)
-        {
-            alert("连接服务器错误!");
+            public=true;
         }
+        var classify=$("#classify").val()
+        
+        data={
+            "token":token,
+            "title":title,
+            "content":content,
+            "classifyID":classify,
+            "public":public,
+        }
+    
+        $.ajax({
+            type:"POST",
+            Accept: "application/json; charset=utf-8",
+            url:host+"v1/blog/article",
+            dataType:"json",
+            data:JSON.stringify(data),
+            success:function(json)
+            {
+                if(json.code==200)
+                {
+                    alert("发布成功!");
+                }
+                else
+                {
+                    alert("发布失败!\n原因:"+json.error)
+                }
+            },error:function(json)
+            {
+                alert("连接服务器错误!");
+            }
+        });
     });
-});
+    
+}
